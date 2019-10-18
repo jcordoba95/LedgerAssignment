e576c3f0f81c68b6ed7e929973120467d7892ac8
Jorge Cordoba <jcordoba@NSL-JCORDOBAs-MacBook-Pro.local>
2019-10-18 09:30:43 -0700

    Need to fix extra currencies

--- run.py
+++ run.py
@@ -23,12 +23,24 @@ def main(price_db, f):
     global ledgerImportsList
     priceDbParser(price_db)
     ledgerImports(f)
+    # Create our tree from every transaction in the '!include' files found in index.ledger
     for line in ledgerImportsList:
         if line.startswith('!include'):
             buildTree(line.split(' ')[1])
             break #Prueba
-    
 
+# Missing Regex portion
+def ledgerPrint():
+    for importLine in ledgerImportsList:
+        if importLine.startswith('!include'):
+            file = open(importLine.split(' ')[1], 'r')
+            fileList = file.read().splitlines()
+            file.close()
+            for line in fileList:
+                if not line.startswith(';'):
+                    print(line)
+
+            
 
 def priceDbParser(file):
     file = open(file, 'r')
@@ -45,18 +57,27 @@ def ledgerImports(f):
 def breadthSearch(elements, node):
     if len(elements) == 0:
         return node
-    
+    if len(node.childs) == 0:
+        n = Node()
+        n.name = elements[0]
+        node.childs.append(n)
+        n.parent = node
+        elements.remove(elements[0])
+        return breadthSearch(elements, n)
+
     for value in node.childs:
         if elements[0] == value.name:
             elements.remove(elements[0])
             return breadthSearch(elements, value)
 
+    # Node doesn't exist yet in breadth.
     n = Node()
     n.name = elements[0]
     node.childs.append(n)
+    n.parent = node
     elements.remove(elements[0])
-    breadthSearch(elements, n)
-            
+    return breadthSearch(elements, n)
+
 
 
         
@@ -64,41 +85,63 @@ def breadthSearch(elements, node):
 
 
 
-def addNode(accounts, ammount):
+def addNode(accounts, amount):
     global root
     accounts = accounts.split(':')
-    print(accounts)
-    node = breadthSearch(accounts, root.childs)
-    node.balance =+ ammount
+    node = breadthSearch(accounts, root)
 
+    node.balance += float(amount[1:])
+
+def subtractFromNode(accounts, amount):
+    global root
+    accounts = accounts.split(':')
+    node = breadthSearch(accounts, root)
+    amount = amount * -1.0
+    # amount = float(amount)
+
+    node.balance += amount
 
 def buildTree(file):
     file = open(file, 'r')
     fileList = file.read().splitlines()
     file.close()
-    flag = ''
+    flag = 'new'
     pastLine = ''
+    totalAmount = 0.0
     for line in fileList:
         if not line.startswith(';'):
-            if line[0].isdigit():
-                # Nuevos datos.
-                # SUBSTRACT from pastLine the double the ammount
+            if line[0].isdigit() and flag == 'sub':
+                # New Data.
+                subtractCorrection(flag, pastLine, totalAmount)   
+                totalAmount = 0.0
                 flag = 'new'
             elif line[0] == '\t':
                 accountsAmount = line.split('\t')
-                pastLine = accountsAmount
+                accounts = accountsAmount[1]
+                # pastLine = accountsAmount
                 if len(accountsAmount) > 3:
                     # This means it has the accounts and the amount
-                    accounts = accountsAmount[1]
                     amount = accountsAmount[3]
+                    totalAmount += float(amount[1:])
                     # Method to add to tree new element
                     addNode(accounts, amount)
-
+                else:
+                    # Subtract to this line what is in     totalAmount.
+                    if totalAmount > 0.0:
+                        subtractFromNode(accounts, totalAmount)
+                    flag = 'sub'
+                # Header of transaction in 'line'
                 # print(accountsAmount)
             pastLine = line
+    subtractCorrection(flag, pastLine, totalAmount)
+    
 
-
-
+def subtractCorrection(flag, pastLine, totalAmount):
+    if flag != 'sub' and totalAmount > 0.0:
+        # SUBSTRACT from pastLine the amount AND total amount.
+        accountsAmount = pastLine.split('\t')
+        accounts = accountsAmount[1]
+        subtractFromNode(accounts, totalAmount)
 
 
 
--- test.py
+++ test.py
@@ -1,10 +1,79 @@
-# 2013/2/20 Purchased reddit gold for the year
-# 	Asset:Bitcoin Wallet		-10 BTC
-# 	Expense:Web Services:Reddit
+import sys
+import re
 
-mytuple = ("apple", "banana", "cherry")
-myit = iter(mytuple)
+priceDbList = []
+ledgerImportsList = []
+transactionData = []
+coinsBalances = {
+    "default": "0"
+}
+# print(sys.argv)
 
-print(next(myit))
-print(next(myit))
-print(next(myit))
+def priceDbParser(file):
+    file = open(file, 'r')
+    global priceDbList
+    priceDbList = file.read().splitlines() 
+    file.close()
+
+def main(arguments):
+    arguments.remove(arguments[0])
+    comesFrom = ''
+    priceLoaded = False
+    indexLoaded = False
+    for arg in arguments:
+        if comesFrom == '--price-db':
+            priceDbFile(arg)
+            priceLoaded = True
+            comesFrom = ''
+            continue
+        if comesFrom == '-f':
+            indexFiles(arg)
+            indexLoaded = True
+            comesFrom = ''
+            continue
+        if arg == '--price-db':
+            comesFrom = '--price-db'
+            continue
+        if arg == '-f':
+            comesFrom = '-f'
+            continue
+    for line in ledgerImportsList:
+        if line.startswith('!include'):
+            includeParser(line.split(' ')[1])
+    if indexLoaded and priceLoaded and len(arguments) > 4:
+        # Read now the other flags
+        print(arguments)
+
+
+def priceDbFile(file):
+    file = open(file, 'r')
+    global priceDbList
+    priceDbList = file.read().splitlines() 
+    file.close()
+
+def indexFiles(file):
+    file = open(file, 'r')
+    global ledgerImportsList
+    ledgerImportsList = file.read().splitlines()
+    file.close()
+
+def includeParser(file):
+    datePattern = '/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/'
+    file = open(file, 'r')
+    rows = file.read().splitlines()
+    for data in rows:
+        if not data.startswith(';'):
+            if data[0].isdigit():
+                
+                regex = re.compile(datePattern)
+                date = data.split(' ')[0]
+                result = regex.search(date)
+                
+                print('asd:')
+                print(result.group(1))
+
+                
+    
+
+if __name__ == '__main__':
+    main(sys.argv)
