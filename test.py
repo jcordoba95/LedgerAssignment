4a173cecdeee71a133510ad7ab9d5f2eee5fe0ac
Jorge Cordoba <jcordoba@NSL-JCORDOBAs-MacBook-Pro.local>
2019-10-18 16:25:32 -0700

    Data structure is now filled

--- test.py
+++ test.py
@@ -1,6 +1,23 @@
 import sys
 import re
 
+class Transaction:
+    def __init__(self):
+        self.date = ""
+        self.description = ""
+        self.postings = []
+
+
+class Posting:
+    def __init__(self):
+        self.account = ""
+        self.balances = {}
+        # "coin"    : amount,
+        # "BTC"     : 0.0,
+        # "$"       : 300.00
+
+
+
 priceDbList = []
 ledgerImportsList = []
 transactionData = []
@@ -40,9 +57,20 @@ def main(arguments):
     for line in ledgerImportsList:
         if line.startswith('!include'):
             includeParser(line.split(' ')[1])
-    if indexLoaded and priceLoaded and len(arguments) > 4:
+
+            
+    if indexLoaded:
         # Read now the other flags
-        print(arguments)
+        if '--sort' in arguments:
+            sort = ''
+            flag = False
+            for arg in arguments:
+                if arg == '--sort':
+                    flag = True
+                    continue
+                if flag:
+                    sort = arg
+        
 
 
 def priceDbFile(file):
@@ -58,19 +86,99 @@ def indexFiles(file):
     file.close()
 
 def includeParser(file):
-    datePattern = '/([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/'
+    datePattern = re.compile(r"\d{4}/\d{1,2}/\d{1,2}")
+    regex = re.compile(datePattern)
     file = open(file, 'r')
     rows = file.read().splitlines()
+    trans = Transaction()
+    total = {}
+    sub = False
     for data in rows:
         if not data.startswith(';'):
             if data[0].isdigit():
-                
-                regex = re.compile(datePattern)
                 date = data.split(' ')[0]
                 result = regex.search(date)
+
+                # Add inverted values from total to the last Posting
+                if sub:
+                    index = len(trans.postings) - 1
+                    trans.postings[index].balances = total
+                    for coin in trans.postings[index].balances:
+                        trans.postings[index].balances[coin] *= -1
+                    # Add current Transaction to our transactions list
+                    transactionData.append(trans)
+                    trans = Transaction()
+                    sub = False
+                total = {}
+
+                if len(trans.postings) == 0:
+                    # First transaction in the iteration
+                    trans.date = result.string
+                    trans.description = data.replace(trans.date, '')
+                else:
+                    # Add current Transaction to our transactions list
+                    transactionData.append(trans)
+                    trans = Transaction()
+                    trans.date = result.string
+                    trans.description = data.replace(trans.date, '')
+            
                 
-                print('asd:')
-                print(result.group(1))
+                # print(result.string)
+            else:
+                # New Posting
+                posting = Posting()
+                # Used to check if the previous char was a blank space
+                flag = False
+
+                account = ""
+                balances = {}
+                data = data[1:]
+                # Iterate to split account from amount
+                for char in data:
+                    if char == '\t':
+                        break
+                    if char == ' ':
+                        if flag:
+                            break
+                        flag = True
+                    elif char.isalpha():
+                        flag = False
+                    account += char
+                    
+                posting.account = account
+                amount = data.replace(account, '')
+                amount = amount.split()
+
+                if len(amount) == 0:
+                    # Add the total negative amount to posting.amount
+                    sub = True
+                    
+                elif len(amount) == 2:
+                    posting.balances[amount[1]] = float(amount[0])
+                    total[amount[1]] = float(amount[0])
+                    
+                    # amount = float(amount[0])
+                elif len(amount) == 1:
+                    number = amount[0].replace('$', '')
+                    posting.balances['$'] = float(number)
+                    total['$'] = float(number)
+
+                    # amount = amount[0].replace('$', '')
+                trans.postings.append(posting)
+    # END FOR
+    
+    # Add last transaction into our Transaction list
+    if sub:
+        index = len(trans.postings) - 1
+        trans.postings[index].balances = total
+        for coin in trans.postings[index].balances:
+            trans.postings[index].balances[coin] *= -1
+    transactionData.append(trans)
+
+            
+
+
+            
 
                 
     
